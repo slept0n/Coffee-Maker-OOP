b6924d99a883069c9bd77f9e5b690c4572cbdc4b
Wes <pugliesewesley@gmail.com>
2020-11-24 18:18:03 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,29 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
+# MenuItem = MenuItem()
+
+is_on = True
+
+coffee_maker.report()
+money_machine.report()
+
+while is_on:
+    options = menu.get_items()
+    choice = input(f"What would you like? ({options})")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        is_enough_ingredients = coffee_maker.is_resource_sufficient(drink)
+        is_payment_successful = money_machine.make_payment(drink)
+        # if these are both true, make the coffee
+        if is_enough_ingredients and is_enough_ingredients:
+            coffee_maker.make_coffee(drink)
